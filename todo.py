56fca53762297a33521eb7b9d3c0d2b238cbbab0
João Pedro <joao.pedro@km.kongsberg.com>
2026-09-04 15:25:10 +0200

    small change to the todo.py

--- todo.py
+++ todo.py
@@ -25,7 +25,7 @@ class TodoHighlighterListener(sublime_plugin.EventListener):
                 "scope": "invalid"          # Usually renders bright red
             },
             "highlight_warning": {
-                "pattern": r"\b(HACK|OPTIMIZE|WARN)\b",
+                "pattern": r"\b(HACK|HARDCODED|WARN)\b",
                 "scope": "markup.changed"   # Usually renders yellow/orange
             }
         }
